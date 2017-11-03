f3ca7e7e38b380ef4cf173c9e8c37850cca82162
Frank J. Reashore <frank.reashore@gov.bc.ca>
2017-11-03 11:24:31 -0700

    Added ttodolist

--- src/Components/TodoList.js
+++ src/Components/TodoList.js
@@ -2,12 +2,35 @@ import React, { Component } from 'react';
 import '../App.css';
 
 class TodoList extends Component {
+    getInitialState() {
+        return {
+            items: []
+        };
+    }
+
+    addItem(e){
+        var itemsArray = this.state.items;
+
+        itemsArray.push(
+            {
+                text: this._inputElement.value,
+                key: Date.now
+            }
+        );
+
+        this.setState({
+            items: itemsArray
+        })
+
+        e.preventDefault();
+    }
+
     render() {
       return (
         <div className="todoListMain">
             <div className="header">
-                <form>
-                    <input placeholder="Enter task"/>
+                <form onSubmit={this.addItem}>
+                    <input ref={(a) => this._inputElement = a} placeholder="Enter task"/>
                     <button type="submit">Add</button>
                 </form>
             </div>
